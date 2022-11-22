5b30f6858a23f5c3825d1c0598999d6838dd2f3f
MyHappySun <112709363+deprEsS-Today@users.noreply.github.com>
2022-11-22 18:56:18 +0300

    completed oop-08
    lab is done. All tasks are completed

---
+++ Boss.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static oop_8.bossAction;
+
+namespace oop_8
+{
+    public delegate void bossAction(string message);
+
+    public class Boss
+    {
+        public event bossAction Notify;
+
+        public int CMS { get; set; }
+        public int stateOfDevice { get; set; }
+        public Boss(int cMS, int stateOfDevice)
+        {
+            CMS = cMS;
+            this.stateOfDevice = stateOfDevice;
+        }
+
+        public void TurnOn(int stateOfDevice)
+        {
+            if (stateOfDevice == 0)
+            {
+                Notify?.Invoke("CMS уже выключена и не работает");
+            }
+            if (stateOfDevice == 1)
+            {
+                Notify?.Invoke("CMS уже включена и работает");
+            }
+            else {
+                Notify?.Invoke("CMS упала, крепитесь");
+                  
+            }
+        }
+        public int bossCondition { get; set; } //почитать про get set 
+        public void Upgrade(int bossCondition, int stateOfDevice) 
+        {
+            if (stateOfDevice == 0) 
+            {
+                Notify?.Invoke("Босс находится на грани нервного срыва, не любит ждать");
+                bossCondition = bossCondition - 1;
+            }
+            if (stateOfDevice == 1)
+            {
+                Notify?.Invoke("Босс в хорошем расположении духа");
+                bossCondition = bossCondition + 1;
+            }
+            else
+            {
+                Notify?.Invoke("Босс сорвался");
+                bossCondition = bossCondition - 3;
+            }
+            if(bossCondition < 1) Notify?.Invoke("Босс всех уволил");
+
+        }
+        public int changer( Boss item, int state)
+        {
+            if (state == 0)
+            {
+                item.stateOfDevice = 0;
+
+            }
+            else if (state == 1)
+            {
+                item.stateOfDevice = 1;
+
+            }
+            else item.stateOfDevice = 3;
+            return stateOfDevice;
+        }
+        public override string ToString()
+        {
+            return $"CMS содержит {CMS} клиентов и имеет состояние {stateOfDevice}";
+        }
+    }
+}
---
+++ Program (8).cs
@@ -0,0 +1,64 @@
+﻿namespace oop_8
+{
+    public class Program
+    {
+        //обработчик событий
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine("-------Уведомление!-------");
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            Boss CMS = new Boss(10, 1);
+            Boss CMS2 = new Boss(100, 2);
+            Boss CMS3 = new Boss(1000, 0);
+            Boss CMS4 = new Boss(0, 1);
+            int bossCondition = 8;
+            Console.WriteLine(CMS.ToString());
+            Console.WriteLine(CMS2.ToString());
+            Console.WriteLine(CMS3.ToString());
+            Console.WriteLine(CMS4.ToString());
+            CMS.Notify += DisplayMessage;
+            CMS2.Notify += DisplayMessage;
+            CMS3.Notify += DisplayMessage;
+            CMS4.Notify += DisplayMessage;
+            CMS.Upgrade(bossCondition, CMS.stateOfDevice );
+            CMS2.Upgrade(bossCondition, CMS2.stateOfDevice );
+            CMS3.Upgrade(bossCondition, CMS2.stateOfDevice);
+            CMS4.Upgrade(bossCondition, CMS4.stateOfDevice );
+            CMS.TurnOn(CMS.stateOfDevice);
+            CMS4.changer( CMS4, 0);
+            CMS3.changer(CMS3, 9);
+            Console.WriteLine(CMS4.stateOfDevice);
+            CMS2.TurnOn(CMS2.stateOfDevice);
+            CMS3.TurnOn(CMS2.stateOfDevice);
+            CMS3.Upgrade(CMS3.bossCondition, CMS3.stateOfDevice);
+            Console.WriteLine(CMS.ToString());
+            Console.WriteLine(CMS2.ToString());
+            Console.WriteLine(CMS3.ToString());
+            Console.WriteLine(CMS4.ToString());
+
+            string changingStr = "Nice String";
+            StrAct lowNup = StringAction.Lower;
+            lowNup += StringAction.Upper;
+            lowNup(changingStr);
+            Predicate<int> isLong = (int x) => x > 5;
+            Console.WriteLine(isLong(changingStr.Length));
+            DoOperation(changingStr, StringAction.Remove);
+            DoOperation(changingStr, StringAction.Insert);
+            void DoOperation(string str, Action<string> op) => op(changingStr);
+            Func<string, string> convertMethod = StringAction.RemoveEleminstr;
+            Console.WriteLine(convertMethod(changingStr));
+            convertMethod += StringAction.Reverse;
+            Console.WriteLine(convertMethod(changingStr));
+            Func<string, string> convert = delegate (string str)
+            { return str.Insert(str.Length, "!!!!!"); };
+            Console.WriteLine(convert(changingStr));
+            string secOperation(string str, Func<string, string> operation) => operation(str);
+            Console.WriteLine(secOperation(changingStr, str => str.Insert(1, "/")));
+        }
+
+    }
+}
